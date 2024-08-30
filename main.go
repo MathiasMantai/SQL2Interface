f20f7b4789f42a67fecd0e90f3662b91550070b2
MathiasMantai <mmantaibusiness@gmail.com>
2024-08-30 16:11:01 +0200

    chore: - panic when input and output are not specified

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"strings"
+	"errors"
 	"flag"
 	c "github.com/MathiasMantai/sql2interface/convert"
 )
 
 func main() {
-
 	output := flag.String("o", "", "source directory to scan for sql files")
 	dir := flag.String("i", "", "target directory to save converted interfaces as typescript files")
 	flag.Parse()
+
+	if strings.TrimSpace(*output) == "" || strings.TrimSpace(*dir) == "" {
+		panic(errors.New("=> input and output have to be specified"))
+	}
+
 	sqlParser := c.NewSQL2Interface(*dir, *output)
 	sqlParser.Run()
 }
