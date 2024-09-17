33519e83ce214816c15aa959be41c125e950b5ca
MathiasMantai <mmantaibusiness@gmail.com>
2024-09-17 11:45:32 +0200

    fix: fixed a bug where struct of combined table was empty after conversion

--- src/ignore.go
+++ src/ignore.go
@@ -5,6 +5,15 @@ import (
 	"strings"
 )
 
+// IsFileIgnored checks if a given file name is present in a list of ignored files.//+
+// It returns true if the file is ignored, and false otherwise.//+
+// //+
+// Parameters://+
+// - fileName: The name of the file to check.//+
+// - ignoreFiles: A slice of strings containing the names of the files to be ignored.//+
+// //+
+// Return://+
+// - A boolean value indicating whether the file is ignored. If true, the file is ignored.//+
 func IsFileIgnored(fileName string, ignoreFiles []string) bool {
 	for _, ignoreFile := range ignoreFiles {
 		if strings.EqualFold(ignoreFile, fileName) {
@@ -16,6 +25,16 @@ func IsFileIgnored(fileName string, ignoreFiles []string) bool {
 	return false
 }
 
+// IsColumnIgnored checks if a given column name is present in a list of ignored columns for a specific file.//+
+// It returns true if the column is ignored, and false otherwise.//+
+// //+
+// Parameters://+
+// - fileName: The name of the file to check.//+
+// - columnName: The name of the column to check.//+
+// - ignoreColumns: A map where the keys are file names and the values are slices of strings containing the names of the columns to be ignored.//+
+// //+
+// Return://+
+// - A boolean value indicating whether the column is ignored. If true, the column is ignored.//+
 func IsColumnIgnored(fileName string, columnName string, ignoreColumns map[string][]string) bool {
 	for _, ignoreColumn := range ignoreColumns[fileName] {
 		if strings.EqualFold(ignoreColumn, columnName) {
